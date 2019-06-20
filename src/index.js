f466cece397a0ba9147ef52a4bc1a005f36a10af
Taine Walford <tainewalford@live.com>
2019-06-20 13:59:42 +1200

    restarting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-//Components
-import App from './client/components/App'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// Firestore
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-admin.initializeApp(functions.config().firebase);
-
-let db = admin.firestore();
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root'))
-})
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
