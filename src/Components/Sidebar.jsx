f9f9021dfadbc4d9ba40a5730e3fb00cc67b22f9
Taine Walford <tainewalford@live.com>
2019-06-20 22:19:00 +1200

    styling

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -8,6 +8,7 @@ export default class Sidebar extends Component {
         {this.props.bois.map(boi => {
           return <>
             <div className='playerContainer'>
+              <div className='selector'></div>
               <div class="lightHeader">{boi.name}</div>
               <span>{boi.win} wins | </span>
               <span>{boi.loss} losses </span>
