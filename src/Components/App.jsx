38d94cd4245e24dbe8004db7b9c83b8330e4e25c
Taine Walford <tainewalford@live.com>
2019-06-29 22:46:12 +1200

    testing

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -31,8 +31,24 @@ export default class App extends Component {
     })
   }
 
-  getData = () => {
-    console.log(this.state.bois)
+  handlePlayerSelection = (colour, player) => { // sets state colour to player clicked
+    let flippedColour =  (this.state[colour] === 'blue') ? 'red' : 'blue'
+    let thisState = this.state[colour].name
+    let otherState = this.state[flippedColour].name
+    // If selected twice, clear other state colour
+    if(otherState === player.name) this.setState({[flippedColour] : {}})
+    // If state colour is occupied, default back
+    if (thisState) {
+      let currentButton = document.getElementById(`${thisState}Selector`)
+      currentButton.className = "default"
+    }
+    // Set new state colour and set class to state colour
+    let currentButton = document.getElementById(`${player.name}Selector`)
+    currentButton.className = colour
+    this.setState({ [colour]: player })
+  }
+
+  getData = () => { // pull data from Firestore
     let arr = []
     const db = fire.firestore()
     db.collection('bois').get()
@@ -43,11 +59,18 @@ export default class App extends Component {
         this.setState({ bois: arr })
       })
   }
+
   render() {
     return (
       <div className="mainContain">
-        <h1 className='center'>T R A C K K E N</h1>
-        <div className="sidebar"><Sidebar bois={this.state.bois} /></div>
+        <div className="leftContain">
+          <h1 className='center'>T R A C K K E N</h1>
+          <div className="matchContain">
+            <div className="matchPlayer blue" player={this.state.blue}>  </div>
+            <div className="matchPlayer red"  player={this.state.red}></div>
+          </div>
+        </div>
+        <div className="sidebar"><Sidebar bois={this.state.bois} handlePlayerSelection={this.handlePlayerSelection} /></div>
       </div>
     )
   }
